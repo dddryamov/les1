478c4af4a2ad554b6216c7e838c3ad99302b5f51
VitalyDryamov <dddryamov@gmail.com>
2022-10-12 16:48:26 +0300

    CONF

---
+++ ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ВВесдите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура Это МАША");
+}
+else
+{
+    Console.WriteLine("Привет " );
+    Console.Write(username);
+}
